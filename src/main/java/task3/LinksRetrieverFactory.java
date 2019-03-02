4cc31d8cd15345ba753e6567f39a76b2e3cde67b
Ott Kärner <ottkarner@gmail.com>
2019-03-02 15:08:24 +0200

    Homework 3 refactoring2

--- src/main/java/task3/LinksRetrieverFactory.java
+++ src/main/java/task3/LinksRetrieverFactory.java
@@ -2,11 +2,11 @@ package task3;
 
 public class LinksRetrieverFactory {
 
-
     public LinksRetriever getLinksRetriever(String linksRetrieverType) {
         if (linksRetrieverType.equalsIgnoreCase("RIIGI_TEATAJA_EE_JSOUP")) {
             return new RiigiTeatajaEeQueryLinksRetrieverByJsoup();
         }
+
         if (linksRetrieverType.equalsIgnoreCase("RIIGI_TEATAJA_EE_SELENIDE")) {
             return new RiigiTeatajaEeQueryLinksRetrieverBySelenide();
         }
